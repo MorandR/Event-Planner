e5e491920c6242c34f4ba63ae5204e2979505d1e
adabs <goddemraw@gmail.com>
2022-04-12 12:48:33 -0400

    Register works as intended with the setup described in notes.txt

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -20,22 +20,56 @@ router.post('/register', async (req, res, next) => {
                     msg: err
                 });
             }
-            // now to insert user with email and password.
-            else {
-                db.query(`INSERT INTO users (email, password, userLevel) VALUES('${req.body.email}', '${req.body.password}', '${req.body.userLevel}')`,
+            // now to insert user with email and password. check userLevel to decide if a univ profile needs to be created
+            else { // if userLevel is NOT super admin, assign user the selected schools id for school_id
+                if (req.body.userLevel != "super admin"){
+                    db.query(`INSERT INTO users (email, password, userLevel, school_id) VALUES('${req.body.email}', '${req.body.password}', '${req.body.userLevel}', (SELECT university_id from university where school_name = '${req.body.school_name}'))`,
+                        (err, result) => {
+                            if (err) {
+                                return res.status(400).send({
+                                    msg: err,
+                                    line: 31,
+                                    school: req.body.school_name
+                                });
+                            } // successful queries above, send success status message.
+                            else{
+                                return res.status(201).send({
+                                    msg: 'User registered.'
+                                })
+                            }
+                        }
+                    )
+                }
+                // if userLevel is super admin, creating a user with the created schools id, also create user after with school_id.
+                else {// first, create the university profile with error checking.
+                    db.query(`INSERT INTO university (location, num_students, school_name) VALUES('${req.body.location}', '${req.body.num_students}', '${req.body.school_name}')`,
+                        (err, result) => {
+                            if (err) {
+                                return res.status(400).send({
+                                    msg: err,
+                                    line: 49
+                                });
+                            }                            
+                        }
+                    )
+                    // if we make it past the above university profile creation, we can then create the user.
+                    db.query(`INSERT INTO users (email, password, userLevel, school_id) VALUES('${req.body.email}', '${req.body.password}', '${req.body.userLevel}', (SELECT university_id from university where school_name = '${req.body.school_name}'))`,
                     (err, result) => {
                         if (err) {
                             return res.status(400).send({
-                                msg: err
+                                msg: err,
+                                line: 60
                             });
-                        } // successful queries above, send success status message.
+                        }
                         else{
                             return res.status(201).send({
                                 msg: 'User registered.'
                             })
-                        }
+                        }                      
                     }
-                )
+                )                                
+
+                }
             }
         }
     )
