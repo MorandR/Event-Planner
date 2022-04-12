569a9253bd63b4c5c8ed2c18601e245bf4bd01df
Richard M <70453767+MorandR@users.noreply.github.com>
2022-04-12 14:41:04 -0400

    Updated register page
    Added extra fields correlating to the user level

--- frontend/src/Auth/RegisterPage.jsx
+++ frontend/src/Auth/RegisterPage.jsx
@@ -1,171 +1,251 @@
-import { Accordion, AccordionSummary, Avatar, Grid, Link, List, ListItem, ListItemButton, ListItemText, TextField, Typography } from "@mui/material";
+import {
+  Accordion,
+  AccordionSummary,
+  Autocomplete,
+  Avatar,
+  Grid,
+  Link,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router";
 
 import CustomButton from "../UI/Button";
 
 export default function RegisterPage(props) {
+  const navigate = useNavigate();
   const [confirmPassword, setConfirmPassword] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
+  const [currentLevel, setCurrentLevel] = useState("Select UserLevel");
 
-  const userLevel = ["student", "admin", "super admin"]
+  const userLevel = ["student", "admin", "super admin"];
   let userSelected = 0;
 
+  const [isSuperAdmin, setIsSuperAdmin] = useState(undefined)
 
-  
+  const [schoolName, setSchoolName] = useState("");
+  const [schooLoc, setSchoolLoc] = useState();
+  const [NumOfStudents, setNumOfStudents] = useState();
+
+  const schoolList = [
+    // Grab list of schools from Database //
+
+    { label: "UCF" },
+    { label: "FSU" },
+    { label: "USF" },
+  ];
 
   const handleRegister = (event) => {
-
     event.preventDefault(); // prevents screen from reloading, which is set to default
 
     const data = {
-      username: username,
       email: email,
       password: password,
-      userLevel: userLevel[userSelected]
-    }
-
+      userLevel: userLevel[userSelected],
+    };
 
     console.log("Enter Response");
     console.log(data);
-    
+
     axios
-    .post("http://localhost:5000/api/register/", data)
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((error) => {
-      console.log("ERROR");
-      console.log(error);
-    });
-
-
-
-  }
+      .post("http://localhost:5000/api/register/", data)
+      .then((res) => {
+        console.log(res);
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.log("ERROR");
+        console.log(error);
+      });
+  };
 
   return (
-    <Grid sx={{height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-    <Box
+    <Grid
       sx={{
+        height: "100vh",
+        width: "100vw",
         display: "flex",
-        flexDirection: "column",
-        alignItems: "center",        
-        borderStyle: "solid",
-        borderColor: "black"
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <Grid container justifyContent="center" alignItems="center">
-        {/* Harcoded bgcolor (Should be changed in the future) */}
-        <Avatar sx={{ bgcolor: "#FE6B8B" }}>
-          {/* <LockIcon /> */}
-        </Avatar>
-      </Grid>
-  
       <Box
-        component="form"
-        onSubmit={handleRegister}
-        noValidate
         sx={{
           display: "flex",
           flexDirection: "column",
-          width: 300,
           alignItems: "center",
+          borderStyle: "solid",
+          borderColor: "black",
         }}
       >
- 
-        <TextField
-          margin="normal"
-          sx={{ borderRadius: 100 }}
-          required
-          fullWidth
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          type="email"
-          onChange={(event) => setEmail(event.target.value)}
-          defaultValue={email}
-          autoFocus
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Confirm Password"
-          type="password"
-          autoComplete="current-password"
-          onChange={(event) => setConfirmPassword(event.target.value)}
-        />
+        <Grid container justifyContent="center" alignItems="center">
+          {/* Harcoded bgcolor (Should be changed in the future) */}
+          <Avatar sx={{ bgcolor: "#FE6B8B" }}>{/* <LockIcon /> */}</Avatar>
+        </Grid>
 
-        <Accordion>
-          <AccordionSummary
-          expandIcon={<ExpandMoreIcon/>}
-          >
-            <Typography> Select UserLevel </Typography>
-          
-          </AccordionSummary>
-
-          <List>
-
-            <ListItem disablePadding>
-              <ListItemButton
-              onClick={() => {userSelected = 0}}>
-              <ListItemText>Student</ListItemText>
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton
-              onClick={() => {userSelected = 1}}>
-              <ListItemText>Admin</ListItemText>
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton
-              onClick={() => {userSelected = 2}}>
-              <ListItemText>Super Admin</ListItemText>
-              </ListItemButton>
-            </ListItem>
-          </List>
-
-        </Accordion>
-
-        <CustomButton
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="secondary"
-          disabled={
-            email === "" ||
-            password === "" ||
-            confirmPassword !== password
-          
-          }
-          sx={{ mt: 3, mb: 3 }}
-        > 
-          Register
-        </CustomButton>
-        <Link
-          href="/login"
-          variant="body2"
+        <Box
+          component="form"
+          onSubmit={handleRegister}
+          noValidate
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            width: 300,
+            alignItems: "center",
+          }}
         >
-          {"Already have an account? Log in!"}
-        </Link>
+          <TextField
+            margin="normal"
+            sx={{ borderRadius: 100 }}
+            required
+            fullWidth
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            type="email"
+            onChange={(event) => setEmail(event.target.value)}
+            defaultValue={email}
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Confirm Password"
+            type="password"
+            autoComplete="current-password"
+            onChange={(event) => setConfirmPassword(event.target.value)}
+          />
+
+          <Accordion style={{ width: 200 }}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              {currentLevel}
+            </AccordionSummary>
+
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton
+                  onClick={() => {
+                    setCurrentLevel("Student");
+                    setIsSuperAdmin(false);
+                    userSelected = 0;
+                  }}
+                >
+                  <ListItemText>Student</ListItemText>
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton
+                  onClick={() => {
+                    setCurrentLevel("Admin");
+                    setIsSuperAdmin(false)
+                    userSelected = 1;
+                  }}
+                >
+                  <ListItemText>Admin</ListItemText>
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton
+                  onClick={() => {
+                    setCurrentLevel("Super Admin");
+                    setIsSuperAdmin(true);
+                    userSelected = 2;
+                  }}
+                >
+                  <ListItemText>Super Admin</ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Accordion>
+
+          {isSuperAdmin == undefined ?
+          
+          <div></div>
+          
+          :
+          (isSuperAdmin ? (
+            <div>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="School Name"
+                label="School"
+                type="tex"
+                autoComplete="school name"
+                onChange={(event) => setSchoolName(event.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="School Location"
+                label="School Location"
+                type="text"
+                autoComplete="School Location"
+                onChange={(event) => setSchoolLoc(event.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="Number of Students"
+                label="Number of Students"
+                type="text"
+                autoComplete="Number of Students"
+                onChange={(event) => setNumOfStudents(event.target.value)}
+              />
+            </div>
+          ) : (
+            <div style={{margin: 10}}>
+              <Autocomplete
+                options={schoolList}
+                renderInput={(params) => (
+                  <TextField {...params} label="School Name" />
+                )}
+                style={{ width: 200 }}
+              />
+            </div>
+          ))}
+
+          <CustomButton
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="secondary"
+            disabled={
+              email === "" || password === "" || confirmPassword !== password
+            }
+            sx={{ mt: 3, mb: 3 }}
+          >
+            Register
+          </CustomButton>
+          <Link href="/login" variant="body2">
+            {"Already have an account? Log in!"}
+          </Link>
+        </Box>
       </Box>
-    </Box>
-  </Grid>
+    </Grid>
   );
 }
