e9f1afdca71644075d8bc767f7c99a26a42b01f9
adabs <goddemraw@gmail.com>
2022-04-13 23:56:52 -0400

    change db_host

--- backend/config/index.js
+++ backend/config/index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const pool = mysql.createPool({
-    host: process.env.HOST,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
